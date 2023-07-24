2028a536d4163a308c43b362f840330c44b70cbb
CallumdGibson <callumgibson06@gmail.com>
2023-07-24 13:06:03 +1200

    Test changes + Frontend Functionality Added

--- cise_ass1a_worksheet2-frontend/src/App.tsx
+++ cise_ass1a_worksheet2-frontend/src/App.tsx
@@ -1,11 +1,33 @@
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Wlecome to CISE - the home of learning and fun</h1>
-    </div>
-  );
+interface State {
+  count: number;
 }
 
+class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  makeIncrementer = (amount: number) => () =>
+    this.setState((prevState: State) => ({
+      count: prevState.count + amount,
+    }));
+
+  increment = this.makeIncrementer(1);
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>
+          Increment count
+        </button>
+      </div>
+    );
+  }
+}
 export default App;
